590702c292c894ff9e19467e449e3e161d0be69c
Danyel Nunes <danyelmaykon@gmail.com>
2020-07-01 12:26:03 -0300

    Inicio do projeto desenvolvido no curso de JavaScript depois da primeira aula

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro programa em JS: trabalhando com variáveis");
+
+const idade = 26; //A foramde criar uma variável nao muda do C#;
+
+console.log(idade); //
+console.log(idade + 2);
+
+const idadeSomada = idade + 2;
+
+console.log(idadeSomada);
