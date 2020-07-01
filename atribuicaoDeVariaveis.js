710392be7424efbd8e6f6ac79d9798b3ba0367c9
Danyel Nunes <danyelmaykon@gmail.com>
2020-07-01 14:22:42 -0300

    Final da ultima aula do capitulo

---
+++ atribuicaoDeVariaveis.js
@@ -0,0 +1,11 @@
+ console.log("Trabalando com Atrivuição de Variáveis");
+
+ const idade = 25;
+ let primeiroNome = "Danyel";
+ const sobrenome = "Nunes";
+
+ console.log(primeiroNome + " " + sobrenome);
+ console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+ const nomeCompleto = primeiroNome + " " + sobrenome;
+ console.log(nomeCompleto);
