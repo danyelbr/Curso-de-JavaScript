4a212f9a83c8a7426d98148461645d40905792df
Danyel Nunes <danyelmaykon@gmail.com>
2020-07-01 12:55:00 -0300

    Informando a importância do nome das variáveis

---
+++ trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+ console.log("Trabalhando com variáveis");
+//JS é case sensitive (maiusculo e minúsculo faz diferença)
+
+const idade = 29; //o nome das variáveis importa muito, por conta das maiúsculas e minúsculos;
+const Idade = 30;
+
+console.log(idade);
+console.log(Idade);
