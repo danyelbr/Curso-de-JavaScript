f04d0fe76f3d9a07f9018c4463b4f2398e7f06f6
Danyel Nunes <danyelmaykon@gmail.com>
2020-07-01 13:16:09 -0300

    Final da primeira aula

---
+++ conversaoDeTipos.js
@@ -0,0 +1,9 @@
+ console.log("Conversão de Tipos");
+
+
+ console.log ("ano"+ " " + 2020);
+ console.log(parseInt("2") + parseInt("2")); //fazendo a conversão explicita dos números para int, no intuito de fazer uma soma.
+ console.log("10" / "2"); //quando fazemos uma operação diferente de soma, ele automaticamente ja faz a conversão implicita.
+ console.log("Danyel" / "2"); //Not a Number.
+ console.log(7 / 2); //trabalhando com ponto flutuante.
+ console.log(6.5); // diferente de 6,5
